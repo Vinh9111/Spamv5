bca38d6f682a3311ee67e5d11e4cf6768937b0a3
Vinh9111 <155311811+Vinh9111@users.noreply.github.com>
2024-03-28 11:54:46 +0700

    Update import.py

--- import.py
+++ import.py
@@ -1,11 +1,1 @@
-import subprocess
 
-def install_packages(packages):
-    for package in packages:
-        try:
-            subprocess.check_call(["pip", "install", package, "--quiet"])
-            print(f"")
-        except subprocess.CalledProcessError as e:
-            print(f"")
-packages = ["colorama", "alive-progress", "requests"]
-install_packages(packages)
